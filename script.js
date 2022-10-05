af3cc393036724fe94d422d43b6ba87bd7bca1a2
Ghamesh <ghamesh441@gmail.com>
2022-10-05 15:57:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ let tdate = daydate + " " + month + " "  + year + " " + dayname;
 
 btn.addEventListener("click", function(){
     let uservalue = document.querySelector("#inputbox").value;
-    let url = ("http://api.weatherstack.com/current?access_key=0aff6a48b44950efc019434728ccf0e0&query=" + uservalue)
+   // let url = ("http://api.weatherstack.com/current?access_key=0aff6a48b44950efc019434728ccf0e0&query=" + uservalue)
 
 
      async function userdata(){
         
 
-        let response = await fetch(url);
+        let response = await fetch("http://api.weatherstack.com/current?access_key=0aff6a48b44950efc019434728ccf0e0&query=" + uservalue);
         let data = await response.json();
         
         let tempra = data.current.temperature;
